697ac66f94a41aa04b16d386fb620f6a226c8bbe
Alex Ionescu <ionescu007@users.noreply.github.com>
2020-02-18 06:15:25 -0800

    Cleanups, and fix a leak
    * Close symlink handle
    * Remove not-needed definitions
    * Cleanup definitions
    * Rename variables / style to match things
    * Add comments
    * Make sure our routine is 64K aligned

--- SymlinkCallback/Main.cpp
+++ SymlinkCallback/Main.cpp
@@ -1,73 +1,66 @@
 #include <ntifs.h>
 #include <wdm.h>
+#include <ntstrsafe.h>
 
 #pragma warning (disable: 4201)
 
-EXTERN_C_START
+#define OBJECT_SYMBOLIC_LINK_USE_CALLBACK 0x10
 
-DRIVER_INITIALIZE DriverEntry;
-DRIVER_UNLOAD DriverUnload;
-
-typedef struct _OBJECT_SYMBOLIC_LINK
-{
-    /* 0x0000 */ union _LARGE_INTEGER CreationTime;
-    union
-    {
-        /* 0x0008 */ struct _UNICODE_STRING LinkTarget;
-        struct
-        {
-            /* 0x0008 */ void* Callback /* function */;
-            /* 0x0010 */ void* CallbackContext;
-        }; /* size: 0x0010 */
-    }; /* size: 0x0010 */
-    /* 0x0018 */ unsigned long DosDeviceDriveIndex;
-    /* 0x001c */ unsigned long Flags;
-    /* 0x0020 */ unsigned long AccessMask;
-    /* 0x0024 */ long __PADDING__[1];
-} OBJECT_SYMBOLIC_LINK, *POBJECT_SYMBOLIC_LINK; /* size: 0x0028 */
-
-NTSYSAPI 
-NTSTATUS 
-NTAPI 
-ObReferenceObjectByName (
-    PUNICODE_STRING ObjectName,
-    ULONG Attributes,
-    PACCESS_STATE AccessState,
-    ACCESS_MASK DesiredAccess,
-    POBJECT_TYPE ObjectType,
-    KPROCESSOR_MODE AccessMode,
-    PVOID ParseContext OPTIONAL,
-    PVOID* Object
-);
-
-__declspec(code_seg(".call$1"))
 NTSTATUS
-SymLinkCallback (
-    _In_ POBJECT_SYMBOLIC_LINK Symlink,
+typedef (SYMLINK_CALLBACK_FUNCTION) (
+    _In_ struct _OBJECT_SYMBOLIC_LINK* Symlink,
     _In_ PVOID SymlinkContext,
     _Out_ PUNICODE_STRING SymlinkPath,
     _Outptr_ PVOID* Object
 );
+typedef SYMLINK_CALLBACK_FUNCTION *PSYMLINK_CALLBACK_FUNCTION;
 
+typedef struct _OBJECT_SYMBOLIC_LINK
+{
+    LARGE_INTEGER CreationTime;
+    union
+    {
+        UNICODE_STRING LinkTarget;
+        struct
+        {
+            PSYMLINK_CALLBACK_FUNCTION Callback;
+            PVOID CallbackContext;
+        };
+    };
+    ULONG DosDeviceDriveIndex;
+    ULONG Flags;
+    ACCESS_MASK AccessMask;
+} OBJECT_SYMBOLIC_LINK, *POBJECT_SYMBOLIC_LINK;
+C_ASSERT(sizeof(OBJECT_SYMBOLIC_LINK) == 0x28);
+
+EXTERN_C_START
+__declspec(code_seg(".call$1")) SYMLINK_CALLBACK_FUNCTION SymLinkCallback;
+DRIVER_INITIALIZE DriverEntry;
+DRIVER_UNLOAD DriverUnload;
 EXTERN_C_END
 
-#define OBJECT_SYMBOLIC_LINK_USE_CALLBACK 0x10
-
-UNICODE_STRING origStr;
-POBJECT_SYMBOLIC_LINK symlinkObj;
+UNICODE_STRING g_LinkPath;
+POBJECT_SYMBOLIC_LINK g_SymLinkObject;
 
 _Use_decl_annotations_
 VOID
 DriverUnload (
     PDRIVER_OBJECT DriverObject
-)
+    )
 {
     UNREFERENCED_PARAMETER(DriverObject);
-    symlinkObj->Flags &= ~OBJECT_SYMBOLIC_LINK_USE_CALLBACK;
-    MemoryBarrier();
-    symlinkObj->LinkTarget = origStr;
 
-    ObDereferenceObject(symlinkObj);
+    //
+    // Undo the patch, restoring the original target string
+    //
+    g_SymLinkObject->Flags &= ~OBJECT_SYMBOLIC_LINK_USE_CALLBACK;
+    MemoryBarrier();
+    g_SymLinkObject->LinkTarget = g_LinkPath;
+
+    //
+    // Dereference the symbolic link object
+    //
+    ObDereferenceObject(g_SymLinkObject);
 }
 
 //
@@ -75,66 +68,80 @@ DriverUnload (
 // we are using a trick:
 // We make sure our callback function is aligned to 64k so
 // while the flags are not yet changed and the callback is still
-// treated as a unicode string, the last 2 bytes are 0000 so the 
+// treated as a unicode string, the last 2 bytes are 0000 so the
 // string length is 0, and the buffer is ignored.
 // To achieve that, we create a section that contains a buffer
-// sized 0xb000 and make sure our callback function is located after it, 
+// sized 0xb000 and make sure our callback function is located after it,
 // and so it is aligned to 64k.
 //
-#pragma section(".call$0",write)
-__declspec(allocate(".call$0")) UCHAR buffer[0xb000] = { 0 };
+#pragma section(".call$0", write)
+__declspec(allocate(".call$0")) UCHAR _pad_[0xB000] = { 0 };
 
 #pragma code_seg(".text")
-
 _Use_decl_annotations_
 NTSTATUS
 DriverEntry (
     PDRIVER_OBJECT DriverObject,
     PUNICODE_STRING RegistryPath
-)
+    )
 {
+    NTSTATUS status;
+    HANDLE symLinkHandle;
+    DECLARE_CONST_UNICODE_STRING(symlinkName, L"\\GLOBAL??\\c:");
+    OBJECT_ATTRIBUTES objAttr = RTL_CONSTANT_OBJECT_ATTRIBUTES(&symlinkName,
+                                                               OBJ_KERNEL_HANDLE |
+                                                               OBJ_CASE_INSENSITIVE);
     UNREFERENCED_PARAMETER(RegistryPath);
 
-    NTSTATUS status;
-    HANDLE symLinkHandle = NULL;
-    UNICODE_STRING symlinkName = RTL_CONSTANT_STRING(L"\\GLOBAL??\\c:");
-    OBJECT_ATTRIBUTES objAttr = RTL_CONSTANT_OBJECT_ATTRIBUTES(&symlinkName, 
-                                                               OBJ_KERNEL_HANDLE | 
-                                                               OBJ_CASE_INSENSITIVE);
+    //
+    // Make sure our alignment trick worked out
+    //
+    if (((ULONG_PTR)SymLinkCallback & 0xFFFF) != 0)
+    {
+        status = STATUS_CONFLICTING_ADDRESSES;
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID,
+                   DPFLTR_ERROR_LEVEL,
+                   "Callback function not aligned correctly!\n");
+        goto Exit;
+    }
 
+    //
+    // Set an unload routine so we can update during testing
+    //
     DriverObject->DriverUnload = DriverUnload;
 
     //
-    // Open a handle to the symbolic link object for C: directory, 
+    // Open a handle to the symbolic link object for C: directory,
     // so we can hook it
     //
-    status = ZwOpenSymbolicLinkObject(&symLinkHandle, 
-                                      SYMBOLIC_LINK_ALL_ACCESS, 
+    status = ZwOpenSymbolicLinkObject(&symLinkHandle,
+                                      SYMBOLIC_LINK_ALL_ACCESS,
                                       &objAttr);
-
     if (!NT_SUCCESS(status))
     {
-        DbgPrintEx(77, 
-                   0, 
-                   "Failed opening symbolic link with error: %x", 
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID,
+                   DPFLTR_ERROR_LEVEL,
+                   "Failed opening symbolic link with error: %lx\n",
                    status);
         goto Exit;
     }
 
     //
-    // Get the symbolic link object 
+    // Get the symbolic link object and close the handle since we
+    // no longer need it
     //
-    status = ObReferenceObjectByHandle(symLinkHandle, 
-                                       SYMBOLIC_LINK_ALL_ACCESS, 
-                                       NULL, 
-                                       KernelMode, 
-                                       (PVOID*)&symlinkObj, 
+    status = ObReferenceObjectByHandle(symLinkHandle,
+                                       SYMBOLIC_LINK_ALL_ACCESS,
+                                       NULL,
+                                       KernelMode,
+                                       (PVOID*)&g_SymLinkObject,
                                        NULL);
+    ObCloseHandle(symLinkHandle, KernelMode);
     if (!NT_SUCCESS(status))
     {
-        DbgPrintEx(77, 
-                   0, 
-                   "Failed referencing symbolic link with error: %x", 
+        DbgPrintEx(DPFLTR_IHVDRIVER_ID,
+                   DPFLTR_ERROR_LEVEL,
+                   "Failed referencing symbolic link with error: %lx\n",
                    status);
         goto Exit;
     }
@@ -143,32 +150,35 @@ DriverEntry (
     // Save the original string that the symlink points to
     // so we can change the object back when we unload
     //
-    origStr = symlinkObj->LinkTarget;
+    g_LinkPath = g_SymLinkObject->LinkTarget;
 
     //
     // Modify the symlink to point to our callback instead of the string
-    // and change the flags so the union will be treated as a callback. 
+    // and change the flags so the union will be treated as a callback.
     // Set CallbackContext to the original string so we can 
-    // return it from the callback and allow the system to run normally. 
+    // return it from the callback and allow the system to run normally.
     //
-    symlinkObj->Callback = SymLinkCallback;
-    symlinkObj->CallbackContext = &origStr;
+    g_SymLinkObject->Callback = SymLinkCallback;
+    g_SymLinkObject->CallbackContext = &g_LinkPath;
     MemoryBarrier();
-    symlinkObj->Flags |= OBJECT_SYMBOLIC_LINK_USE_CALLBACK;
+    g_SymLinkObject->Flags |= OBJECT_SYMBOLIC_LINK_USE_CALLBACK;
 
 Exit:
+    //
+    // Return the result back to the system
+    //
     return status;
 }
 
-#pragma section(".call$1",execute)
-
+#pragma section(".call$1", execute)
 __declspec(code_seg(".call$1"))
 NTSTATUS
 SymLinkCallback (
     _In_ POBJECT_SYMBOLIC_LINK Symlink,
     _In_ PVOID SymlinkContext,
     _Out_ PUNICODE_STRING SymlinkPath,
-    _Outptr_ PVOID* Object)
+    _Outptr_ PVOID* Object
+    )
 {
     UNREFERENCED_PARAMETER(Symlink);
 
@@ -178,7 +188,6 @@ SymLinkCallback (
     // It's a lot easier to get the string, so we can set Object to Null. 
     //
     *Object = NULL;
-    *SymlinkPath = *(PUNICODE_STRING)(SymlinkContext);  // OrigStr
-
+    *SymlinkPath = *(PUNICODE_STRING)(SymlinkContext);
     return STATUS_SUCCESS;
 }
